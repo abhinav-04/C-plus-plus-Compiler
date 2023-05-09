53fc1559aff51dcaa60c3bc60c2775ce51f11118
Devansh <73755529+Devanxh@users.noreply.github.com>
2023-05-09 13:08:19 +0530

    Update parser.js

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -12,24 +12,66 @@ function parse(tokens) {
     ["<keyword>", "<identifier>", "<delimiter>"],
   ];
 
-  let state = false;
+  if (tokens.length > correctSyntax[0].length) {
+    throw new Error("Syntax error: Too many tokens.");
+  }
 
-  if (tokens.length > correctSyntax[0].length) return false;
-
-  for (let syntax of correctSyntax) {
-    for (let j = 0; j < syntax.length; j++) {
-      try {
-        state = tokens[j] === syntax[j];
-        if (!state) break;
-      } catch (e) {
-        state = false;
+  let stack = [];
+  for (let i = 0; i < tokens.length; i++) {
+    const token = tokens[i];
+    if (token === "{") {
+      stack.push("{");
+    } else if (token === "}") {
+      if (stack.length === 0) {
+        throw new Error("Syntax error: Missing opening brace.");
+      } else {
+        stack.pop();
+      }
+    } else if (token === "print") {
+      const nextToken = tokens[i + 1];
+      if (nextToken === "(") {
+        let j = i + 2;
+        while (j < tokens.length && tokens[j] !== ")") {
+          if (tokens[j] === ";" || tokens[j] === "{") {
+            throw new Error("Syntax error: Attempting to print variable without declaring it.");
+          }
+          j++;
+        }
+        if (j === tokens.length) {
+          throw new Error("Syntax error: Missing closing parenthesis.");
+        }
+      } else {
+        throw new Error("Syntax error: Missing opening parenthesis.");
+      }
+    } else if (token === ";") {
+      const prevToken = tokens[i - 1];
+      if (prevToken !== ">" && prevToken !== "<" && prevToken !== "=") {
+        throw new Error("Syntax error: Missing semicolon.");
       }
     }
-    if (state) {
-      break;
+  }
+
+  if (stack.length > 0) {
+    throw new Error("Syntax error: Missing closing brace.");
+  }
+
+  for (let syntax of correctSyntax) {
+    if (tokens.length !== syntax.length) {
+      continue;
+    }
+
+    let validSyntax = true;
+    for (let i = 0; i < syntax.length; i++) {
+      if (tokens[i] !== syntax[i]) {
+        validSyntax = false;
+        break;
+      }
+    }
+
+    if (validSyntax) {
+      return true;
     }
   }
-  return state;
-}
 
-export default parse;
+  throw new Error("Syntax error: Invalid token sequence.");
+}
