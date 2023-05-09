f64fc9b60bb46792668678d3a12b9ea5c0e4558b
Devansh <73755529+Devanxh@users.noreply.github.com>
2023-05-09 12:49:52 +0530

    Update tokenizer.js

--- src/utils/tokenizer.js
+++ src/utils/tokenizer.js
@@ -23,7 +23,7 @@ function tokenize(lexemes) {
       lexeme === "false"
     ) {
       if (lexeme.length > 10) {
-        console.error(`Exceeded length of numeric constant or identifier: ${lexeme}`);
+        prompt(`Exceeded length of numeric constant or identifier: ${lexeme}`);
       }
       tokens.push("<value>");
     } else if (lexeme.includes(";")) {
@@ -31,11 +31,11 @@ function tokenize(lexemes) {
     } else {
       if (/^[a-zA-Z_]+[a-zA-Z0-9_]*$/.test(lexeme)) {
         if (lexeme.length > 15) {
-          console.error(`Exceeded length of identifier: ${lexeme}`);
+          prompt(`Exceeded length of identifier: ${lexeme}`);
         }
         tokens.push("<identifier>");
       } else {
-        console.error(`Spelling error: ${lexeme}`);
+        prompt(`Spelling error: ${lexeme}`);
       }
     }
   }
